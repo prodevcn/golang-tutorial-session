b7ee4143c385333573b934ee7b3aa55b52bf88fb
block-hac <cn.corestar@gmail.com>
2022-07-21 23:42:16 +0800

    add type casting

---
+++ type_casting.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("- Type Casting")
+
+	var sum int = 17
+	var count int = 5
+	var mean float32
+	
+	mean = float32(sum) / float32(count)
+	fmt.Printf("Value of mean : %f \n", mean)
+}
