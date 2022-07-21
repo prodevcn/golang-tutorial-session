f9ed8e176e55f2a8727a0c197c21d73d98702336
block-hac <cn.corestar@gmail.com>
2022-07-22 01:31:22 +0800

    add interfaces

---
+++ interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt" 
+	"math"
+)
+
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x,y,radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func(circle Circle) area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+func(rect Rectangle) area() float64 {
+	return rect.width * rect.height
+}
+
+func getArea(shape Shape) float64 {
+	return shape.area()
+}
+
+func main() {
+	fmt.Println("- Interfaces -")
+
+	circle := Circle{x: 0, y: 0, radius: 5}
+	rect := Rectangle{width: 10, height: 5}
+
+	fmt.Printf("Circle area: %f\n", getArea(circle))
+	fmt.Printf("Rectangle area: %f \n", getArea(rect))
+
+}
