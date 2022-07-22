ae36484f722b04122c4e95e2ecbef752e1950408
block-hac <cn.corestar@gmail.com>
2022-07-23 00:30:18 +0800

    update array

--- arrays.go
+++ arrays.go
@@ -22,4 +22,38 @@ func main() {
 	for j = 0; j < 10; j++ {
 		fmt.Printf("Element[%d] = %d\n", j, n[j])
 	}
+
+	// multi-dimensional array
+	var a = [5][2]int{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+	var k, h int
+
+	for k = 0; k< 5; k++ {
+		for j = 0; j < 2; j++ {
+			fmt.Printf("a[%d][%d] = %d \n", k, h, a[k][h])
+		}
+	}
+
+	// passing array as parameter
+	var balance = []int{1000, 2, 3, 17, 50}
+	var avg float32
+
+	avg = getAverage(balance, len(balance))
+	
+	fmt.Printf("Average value is %f \n", avg)
+
+}
+
+// passing array as function parameter
+func getAverage(arr []int, size int) float32 {
+	var i int
+	var avg, sum float32
+
+	for i = 0; i< size; i++ {
+		sum += float32(arr[i])
+	}
+
+	fmt.Printf("Sum inside of getAverage is : %f \n", sum)
+
+	avg = sum / float32(size)
+	return avg
 }
