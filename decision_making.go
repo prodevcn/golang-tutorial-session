2087d56cea6506221a7ee290d9f5ab04d45707aa
block-hac <cn.corestar@gmail.com>
2022-07-22 17:38:26 +0800

    add decision making - select

--- decision_making.go
+++ decision_making.go
@@ -34,11 +34,16 @@ func main() {
 	}
 
 	switch {
-	case grade == "A": fmt.Printf("Excellent! \n")
-	case grade == "B": fmt.Printf("Well done \n")
-	case grade == "C": fmt.Printf("You passed \n")
-	case grade == "D": fmt.Printf("Bettr try again \n")
-	default: fmt.Printf("Invalid grade \n")
+	case grade == "A":
+		fmt.Printf("Excellent! \n")
+	case grade == "B":
+		fmt.Printf("Well done \n")
+	case grade == "C":
+		fmt.Printf("You passed \n")
+	case grade == "D":
+		fmt.Printf("Bettr try again \n")
+	default:
+		fmt.Printf("Invalid grade \n")
 	}
 
 	// type swtich
@@ -58,4 +63,22 @@ func main() {
 		fmt.Printf("don't know the type")
 	}
 
+	// select
+	var c1, c2, c3 chan int
+	var i1, i2 int
+
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received", i1, "from c1 \n")
+	case c2 <- i2:
+		fmt.Printf("sent", i2, "from c2 \n")
+	case i3, ok := (<-c3):
+		if ok {
+			fmt.Printf("received ", i3, " from c3 \n")
+		} else {
+			fmt.Printf("c3 is closed \n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
 }
