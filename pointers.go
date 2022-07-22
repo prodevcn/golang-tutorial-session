7b3d6d4b6f50075aa8ca9c3ae1b4fca95f421777
block-hac <cn.corestar@gmail.com>
2022-07-23 01:19:40 +0800

    update pointers

--- pointers.go
+++ pointers.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const MAX int = 3
+
 func main() {
 	fmt.Println("- Pointers -")
 
@@ -17,4 +19,44 @@ func main() {
 	var ptr *int
 	fmt.Printf("The value of ptr is : %x\n", ptr)
 
+	// array of pointer
+	score := []int{10, 100, 200}
+	var i int
+	var ptrs [MAX]*int
+
+	for i = 0; i < MAX; i++ {
+		ptrs[i] = &score[i]
+	}
+
+	for i = 0; i < MAX; i++ {
+		fmt.Printf("Value of a[%d] = %d\n", i, *ptrs[i])
+	}
+
+	// a pointer to a pointer
+	var x int
+	var pointer *int
+	var pointter **int
+
+	x = 3000
+
+	pointer = &x
+	pointter = &pointer
+
+	fmt.Printf("Value of x = %d\n", x )
+	fmt.Printf("Value available at *pointer = %d\n", *pointer )
+	fmt.Printf("Value available at **pointter = %d\n", **pointter)
+
+	// passing pointers to function
+	var m int = 100
+	var n int = 200
+
+	swap(&m, &n)
+
+}
+
+func swap(x *int, y *int) {
+	var temp int
+	temp = *x
+	*x = *y
+	*y = temp
 }
